b2cd410c4021730181e2e76dfcf0940bb36e7d4e
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-05-26 22:53:54 +0600

    files added

---
+++ src/components/header/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FaBell, FaUser } from 'react-icons/fa'
+import { FaSignalMessenger } from 'react-icons/fa6'
+import { RiLayoutGrid2Fill } from 'react-icons/ri'
+
+const Header = () => {
+    return (
+        <header className='py-3'>
+            <div className="container">
+                <div className="flex justify-between items-center gap-2">
+                    <a href="./" className='text-2xl font-bold'>
+                        <span className='text-blue-500'>
+                            Fake
+                        </span>
+                        <span className='text-black'>
+                            book
+                        </span>
+                    </a>
+
+                    <div className="flex justify-end items-center gap-4">
+                        <a href="./" className='text-gray-700 hover:text-gray-900 avatar w-[50px] h-[50px] inline-flex rounded-full text-xl justify-center items-center'>
+                            <RiLayoutGrid2Fill />
+                        </a>
+                        <a href="./" className='text-gray-700 hover:text-gray-900 avatar w-[50px] h-[50px] inline-flex rounded-full text-xl justify-center items-center'>
+                            <FaSignalMessenger />
+                        </a>
+                        <a href="./" className='text-gray-700 hover:text-gray-900 avatar w-[50px] h-[50px] inline-flex rounded-full text-xl justify-center items-center'>
+                            <FaBell />
+                        </a>
+                        <a href="./" className='text-gray-700 hover:text-gray-900 avatar bg-gray-200 w-[50px] h-[50px] inline-flex rounded-full text-xl justify-center items-center'>
+                            <FaUser />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
